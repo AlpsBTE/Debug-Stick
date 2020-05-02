b327e500700e2f854481aa187b4756157210c599
DolphinTech <mcdolphintech@gmail.com>
2020-05-02 10:43:14 +0200

    Made a light version that only requires server side installation

--- src/main/java/com/tfar/debugstick/DebugStick.java
+++ src/main/java/com/tfar/debugstick/DebugStick.java
@@ -9,31 +9,28 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
+
 @Mod.EventBusSubscriber(modid = DebugStick.MODID)
-@Mod(modid = DebugStick.MODID, name = DebugStick.NAME, version = DebugStick.VERSION)
-public class DebugStick
-{
+@Mod(modid = DebugStick.MODID, name = DebugStick.NAME, version = DebugStick.VERSION, acceptableRemoteVersions="*")
+public class DebugStick {
     public static final String MODID = "debugstick";
     public static final String NAME = "Debug Stick";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.0.0";
+    public static final String ITEM_ID = "stick";
 
     private static Logger logger;
-    @SidedProxy(clientSide = "com.tfar."+MODID+".ClientProxy", serverSide = "com.tfar."+MODID+".CommonProxy")
-    public static CommonProxy proxy;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        Utility.register(event.getRegistry());
+        event.getRegistry().register(new ItemDebugStick(ITEM_ID));
     }
 }
--- src/main/java/com/tfar/debugstick/ItemDebugStick.java
+++ src/main/java/com/tfar/debugstick/ItemDebugStick.java
@@ -33,85 +33,48 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.tfar.debugstick.Utility.itemDebugStick;
+import static com.tfar.debugstick.DebugStick.ITEM_ID;
 
 @Mod.EventBusSubscriber(modid = DebugStick.MODID)
 public class ItemDebugStick extends Item {
+
   public ItemDebugStick(String name) {
-    setRegistryName(new ResourceLocation(DebugStick.MODID, name));
-    setTranslationKey(getRegistryName().toString());
-    setCreativeTab(CreativeTabs.TOOLS);
+    setRegistryName(new ResourceLocation("minecraft"/* DebugStick.MODID */, name));
+    setTranslationKey(ITEM_ID);
+    setCreativeTab(CreativeTabs.MISC);
   }
 
-  //called when right clicking a block
-
-  @Override
-  @Nonnull
-  public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-    if (world.isRemote)return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-    ItemStack stack = player.getHeldItemMainhand();
-    NBTTagCompound nbt = stack.getTagCompound();
-    if (nbt == null)Utility.resetNBT(stack);
-    int mode = nbt.getInteger("mode");
-    switch (mode){
-      case 0: {nbt.setString("Blockstate",world.getBlockState(pos).toString());break;}
-      case 1: {
-        TileEntity tile = world.getTileEntity(pos);
-        if (tile == null)break;
-        nbt.setString("NBTTagCompound",tile.writeToNBT(new NBTTagCompound()).toString());break;}
-      case 2:{
-        nbt.setString("BlockstateList",world.getBlockState(pos).getBlock().getBlockState().getValidStates().toString());break;
-      }
-      case 3:{
-        ImmutableList list = world.getBlockState(pos).getBlock().getBlockState().getValidStates();
-        int index = list.indexOf(world.getBlockState(pos));
-        int newState = (index + 1 >= list.size()) ? 0 : index + 1;
-        world.setBlockState(pos,(IBlockState)list.get(newState));break;
-      }
-      case 4:{
-        String blockstateInfo = CraftTweakerMC.getBlockState(world.getBlockState(pos)).toString();
-        nbt.setString("CTBlockstate", blockstateInfo);
-        player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Blockstate " + TextFormatting.GREEN + blockstateInfo));
-        List<String> oredicts = Utility.getOreDictOfItem(new ItemStack(Item.getItemFromBlock(world.getBlockState(pos).getBlock()),1,world.getBlockState(pos).getBlock().getMetaFromState(world.getBlockState(pos))));
-        if (!oredicts.isEmpty())
-        player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Oredicts " + TextFormatting.AQUA + oredicts));
-        else player.sendMessage(new TextComponentString(TextFormatting.RED + "No oredicts found"));
-        StringSelection stringSelection = new StringSelection(blockstateInfo);
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clipboard.setContents(stringSelection, null);
-        break;
-      }
-    }
-    return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-  }
+  // called when right clicking a block
 
   @Override
   @Nonnull
-  public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player,@Nonnull EnumHand hand) {
-    if (world.isRemote)return ActionResult.newResult(EnumActionResult.FAIL,player.getHeldItem(hand));
-    if (player.isSneaking() && world.getBlockState(player.rayTrace(10,1).getBlockPos()).getBlock().isAir(world.getBlockState(player.rayTrace(10,1).getBlockPos()),world,player.rayTrace(5,1).getBlockPos()))Utility.changeMode(player);
-    return super.onItemRightClick(world, player, hand);
+  public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing,
+      float hitX, float hitY, float hitZ) {
+
+    if (world.isRemote)
+      return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    
+    ItemStack stack = player.getHeldItem(hand);
+    NBTTagCompound nbt = stack.getTagCompound();
+    if (nbt == null) {
+      NBTTagCompound empty = new NBTTagCompound();
+      stack.setTagCompound(empty);
+    }
+    if (nbt.getBoolean("isDebugStick")) {
+
+      //fetches all possible block states of the targetted block
+      ImmutableList<IBlockState> states = world.getBlockState(pos).getBlock().getBlockState().getValidStates();
+      int index = states.indexOf(world.getBlockState(pos));
+      int maxIndex = states.size();
+      //go to the next or previous block state depending if the player is sneaking or not and eventually wrap around.
+      int newIndex = (maxIndex + index + (player.isSneaking() ? -1 : 1) % maxIndex) % maxIndex;
+      IBlockState newState = states.get(newIndex);
+      //apply the new block state without triggering block updates
+      world.setBlockState(pos, newState, 2);
+      player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + newState.toString()), true);     
+    }
+    return EnumActionResult.SUCCESS;
+  
   }
 
-  @Override
-  @SideOnly(Side.CLIENT)
-  public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-    if (worldIn == null)return;
-    NBTTagCompound nbt = stack.getTagCompound();
-    if (nbt == null)return;
-    int mode = nbt.getInteger("mode");
-    switch (mode){
-      case 0: {tooltip.add("Blockstate: "+nbt.getString("Blockstate"));break;}
-      case 1: {tooltip.add("NBTTagCompound: "+nbt.getString("NBTTagCompound"));break;}
-      case 2: {tooltip.add("Blockstate List: "+nbt.getString("BlockstateList"));break;}
-      case 3: {tooltip.add("Transform");break;}
-      case 4: {tooltip.add("CT Blockstate:"+nbt.getString("CTBlockstate"));break;}
-      case 5: {tooltip.add("CT Entity:"+nbt.getString("CTEntity"));break;}
-    }
-    super.addInformation(stack, worldIn, tooltip, flagIn);
-  }
 }
-
-
-
-
