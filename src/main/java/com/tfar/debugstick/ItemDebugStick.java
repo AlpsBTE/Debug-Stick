f82a4d06f4bc18069751301c35047b3b88814412
DolphinTech <mcdolphintech@gmail.com>
2020-05-05 19:16:16 +0200

    added left click detection and property selection

--- src/main/java/com/tfar/debugstick/ItemDebugStick.java
+++ src/main/java/com/tfar/debugstick/ItemDebugStick.java
@@ -1,17 +1,13 @@
 package com.tfar.debugstick;
 
 import com.google.common.collect.ImmutableList;
-import crafttweaker.api.minecraft.CraftTweakerMC;
-import crafttweaker.mc1120.commands.CommandUtils;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
@@ -21,22 +17,20 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraftforge.oredict.OreDictionary;
-
-import java.awt.datatransfer.*;
-import java.awt.Toolkit;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collection;
 
 import static com.tfar.debugstick.DebugStick.ITEM_ID;
 
 @Mod.EventBusSubscriber(modid = DebugStick.MODID)
 public class ItemDebugStick extends Item {
+
+  private static final String TAG_SELECTED = "selectedProperty";
+  private static final String TAG_IS_DEBUG_STICK = "isDebugStick";
 
   public ItemDebugStick(String name) {
     setRegistryName(new ResourceLocation("minecraft"/* DebugStick.MODID */, name));
@@ -44,7 +38,7 @@ public class ItemDebugStick extends Item {
     setCreativeTab(CreativeTabs.MISC);
   }
 
-  // called when right clicking a block
+  // called when right-clicking a block
 
   @Override
   @Nonnull
@@ -53,28 +47,106 @@ public class ItemDebugStick extends Item {
 
     if (world.isRemote)
       return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-    
+
     ItemStack stack = player.getHeldItem(hand);
     NBTTagCompound nbt = stack.getTagCompound();
     if (nbt == null) {
       NBTTagCompound empty = new NBTTagCompound();
       stack.setTagCompound(empty);
     }
-    if (nbt.getBoolean("isDebugStick")) {
+    if (nbt.getBoolean(TAG_IS_DEBUG_STICK)) {
 
-      //fetches all possible block states of the targetted block
-      ImmutableList<IBlockState> states = world.getBlockState(pos).getBlock().getBlockState().getValidStates();
-      int index = states.indexOf(world.getBlockState(pos));
-      int maxIndex = states.size();
-      //go to the next or previous block state depending if the player is sneaking or not and eventually wrap around.
-      int newIndex = (maxIndex + index + (player.isSneaking() ? -1 : 1) % maxIndex) % maxIndex;
-      IBlockState newState = states.get(newIndex);
-      //apply the new block state without triggering block updates
-      world.setBlockState(pos, newState, 2);
-      player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + newState.toString()), true);     
+      IBlockState blockstate = world.getBlockState(pos);
+      IProperty selected = getSelectedProperty(blockstate, nbt, player);
+
+      if (selected == null) {
+        player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "This block has no properites"), true);
+      } else {
+
+        blockstate = blockstate.withProperty(selected,
+            cycle(selected.getAllowedValues(), blockstate.getValue(selected), player.isSneaking() ? -1 : 1));
+
+        player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "'" + selected.getName() + "' is now '"
+            + blockstate.getValue(selected).toString() + "'"), true);
+        world.setBlockState(pos, blockstate, 2);
+      }
+
     }
     return EnumActionResult.SUCCESS;
-  
+
+  }
+
+  // called when left-clicking a block
+  @Override
+  public boolean canDestroyBlockInCreative(World world, BlockPos pos, ItemStack stack, EntityPlayer player) {
+
+    NBTTagCompound nbt = stack.getTagCompound();
+    if (nbt == null) {
+      NBTTagCompound empty = new NBTTagCompound();
+      stack.setTagCompound(empty);
+
+    }
+
+    if (nbt.getBoolean(TAG_IS_DEBUG_STICK)) {
+      if (world.isRemote)
+        return false;
+
+      IBlockState blockstate = world.getBlockState(pos);
+      IProperty selected = getSelectedProperty(blockstate, nbt, player);
+
+      if (selected == null) {
+        player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "This block has no properites"), true);
+
+      } else {
+
+        selected = cycle(blockstate.getBlock().getBlockState().getProperties(), selected, player.isSneaking() ? -1 : 1);
+        nbt.setString(TAG_SELECTED, selected.getName());
+        player.sendStatusMessage(
+            new TextComponentString(TextFormatting.WHITE + "Selected property '" + selected.getName() + "'"), true);
+
+      }
+      return false;
+    }
+
+    return true;
+  }
+
+  // returns the property selected in the TAG_SELECTED NBT field, or the
+  // first property or null if there are no properties
+  @Nullable
+  private static IProperty<?> getSelectedProperty(IBlockState blockstate, NBTTagCompound nbt, EntityPlayer player) {
+    if (blockstate.getBlock().getBlockState().getProperties().isEmpty()) {
+
+      return null;
+    } else if (!nbt.hasKey(TAG_SELECTED)) {
+
+      return blockstate.getBlock().getBlockState().getProperties().iterator().next();
+    } else {
+
+      String name = nbt.getString(TAG_SELECTED);
+      ArrayList<IProperty<?>> properties = new ArrayList<>(blockstate.getBlock().getBlockState().getProperties());
+      for (IProperty<?> property : properties) {
+        if (property.getName().equals(name)) {
+
+          return property;
+        }
+      }
+
+      return properties.get(0);
+    }
+
+  }
+
+  // go to the next or previous elemnt depending on the movement
+  // not and eventually wrap around.
+  @Nonnull
+  private static <T> T cycle(Collection<T> possibleValues, T current, int move) {
+    ArrayList<T> elements = new ArrayList<>(possibleValues);
+    int index = elements.indexOf(current);
+    int maxIndex = elements.size();
+
+    int newIndex = (maxIndex + index + move % maxIndex) % maxIndex;
+    return elements.get(newIndex);
   }
 
 }
